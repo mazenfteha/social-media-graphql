3771c8a2c96d2643b51724a87a2279648f29fc1e
mazenfteha <mazn34903@gmail.com>
2024-08-27 17:37:00 +0300

    Validate inputs and DTO's

--- src/auth/dto/login-user.input.ts
+++ src/auth/dto/login-user.input.ts
@@ -1,11 +1,18 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 
 @InputType()
 export class LoginUserInput {
     @Field()
+    @IsNotEmpty()
+    @IsEmail({}, { message: 'Email must be valid' })
     email: string;
 
+    
     @Field()
+    @IsNotEmpty()
+    @IsString()
+    @Length(6, 20, { message: 'Password must be between 6 and 20 characters' })
     password: string;
 }
--- src/auth/dto/register-user.input.ts
+++ src/auth/dto/register-user.input.ts
@@ -1,14 +1,21 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 
 @InputType()
 export class RegisterUserInput {
     @Field()
+    @IsString()
     name: string;
     
     @Field()
+    @IsEmail({}, { message: 'Email must be valid' })
+    @IsNotEmpty()
     email: string;
 
     @Field()
+    @IsNotEmpty()
+    @IsString()
+    @Length(6, 20, { message: 'Password must be between 6 and 20 characters' })
     password: string;
 }
